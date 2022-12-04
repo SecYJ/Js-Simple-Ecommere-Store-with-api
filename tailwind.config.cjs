c5e05e2b26ea10b35a73cdf0c073a366f621137b
Sec <qianqian1903@gmail.com>
2022-12-05 01:19:43 +0800

    testing deployment

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+    // content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+    content: ["*.html", "*.js"],
     theme: {
         container: {
             center: true,
